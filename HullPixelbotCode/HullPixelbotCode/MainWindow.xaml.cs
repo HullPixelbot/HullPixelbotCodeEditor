04fc19344c41005fdfbd3e3ed4261b84faacd455
Rob Miles <rob@robmiles.com>
2017-01-12 20:09:55 +0000

    Complete UI
    Added program control and fixed upload.

--- HullPixelbotCode/HullPixelbotCode/MainWindow.xaml.cs
+++ HullPixelbotCode/HullPixelbotCode/MainWindow.xaml.cs
@@ -51,6 +51,81 @@ namespace HullPixelbotCode
             }
         }
 
+        SerialPort outputPort = null;
+
+        void connectSerial()
+        {
+            string[] portNames = SerialPort.GetPortNames();
+
+            if (portNames.Length == 0)
+            {
+                MessageBox.Show("No serial ports available", "Send program file");
+                return;
+            }
+
+            // See if the selected port is still available
+
+            string selectedPort = portsComboBox.SelectedItem as string;
+
+            if (selectedPort == null)
+            {
+                MessageBox.Show("No serial port selected", "Send program file");
+                return;
+            }
+
+            bool foundPort = false;
+            selectedPort = selectedPort.ToUpper();
+
+            foreach (string port in portNames)
+            {
+                if (port.ToUpper() == selectedPort)
+                {
+                    foundPort = true;
+                    break;
+                }
+            }
+
+            if (!foundPort)
+            {
+                MessageBox.Show("The selected port is no longer available", "Send program file");
+                return;
+            }
+
+            addTextToMonitor("Connecting to " + selectedPort);
+
+            addLineOfTextToMonitor("");
+
+            if (outputPort != null)
+            {
+                addLineOfTextToMonitor("Closing existing port");
+                outputPort.Close();
+            }
+
+            outputPort = new SerialPort(selectedPort, 9600, Parity.None);
+
+            outputPort.DiscardNull = false;
+
+            outputPort.DataReceived += OutputPort_DataReceived;
+
+            outputPort.Open();
+
+            return;
+        }
+
+        void disconnectSerial()
+        {
+            if (outputPort != null)
+            {
+                addLineOfTextToMonitor("Closing serial port");
+                outputPort.Close();
+                outputPort = null;
+            }
+            else
+            {
+                addLineOfTextToMonitor("No serial port to close");
+            }
+        }
+
         void loadFile()
         {
             OpenFileDialog loadDialog = new OpenFileDialog();
@@ -78,113 +153,117 @@ namespace HullPixelbotCode
         void resetMonitor()
         {
             // Clear the monitor window
-            serialDataTextBlock.Text = "";
+            serialDataTextBox.Text = "";
         }
 
         void addLineOfTextToMonitor(string message)
         {
-            serialDataTextBlock.Dispatcher.BeginInvoke(
+            serialDataTextBox.Dispatcher.BeginInvoke(
                 System.Windows.Threading.DispatcherPriority.Normal,
                 new Action(
                 delegate ()
                 {
-                    serialDataTextBlock.Text = serialDataTextBlock.Text + System.Environment.NewLine + message;
+                    serialDataTextBox.Text = serialDataTextBox.Text + System.Environment.NewLine + message;
                 }
             ));
         }
 
         void addTextToMonitor(string message)
         {
-            serialDataTextBlock.Dispatcher.BeginInvoke(
+            serialDataTextBox.Dispatcher.BeginInvoke(
                 System.Windows.Threading.DispatcherPriority.Normal,
                 new Action(
                 delegate ()
                 {
-                    serialDataTextBlock.Text = serialDataTextBlock.Text + message;
+                    serialDataTextBox.Text = serialDataTextBox.Text + message;
                 }
             ));
         }
 
+        const int TERMINATOR = 0x00;
+
         void sendFile()
         {
             // See of we have a port we can use
-            string[] portNames = SerialPort.GetPortNames();
 
-
-            if(portNames.Length == 0)
+            if(outputPort == null)
             {
-                MessageBox.Show("No serial ports available", "Send program file");
-                return;
-            }
-
-            // See if the selected port is still available
-
-            string selectedPort = portsComboBox.SelectedItem as string;
-
-            if(selectedPort == null)
-            {
-                MessageBox.Show("No serial port selected", "Send program file");
-                return;
-            }
-
-            bool foundPort = false;
-            selectedPort = selectedPort.ToUpper();
-
-            foreach (string port in portNames)
-            {
-                if(port.ToUpper() == selectedPort)
-                {
-                    foundPort = true;
-                    break;
-                }
-            }
-
-            if(!foundPort)
-            {
-                MessageBox.Show("The selected port is no longer available", "Send program file");
+                MessageBox.Show("No serial port connected. Select a port and press the Connect button", "Program transfer");
                 return;
             }
 
             // Got a working port - try to send the file
 
-            addLineOfTextToMonitor("Connecting to " + selectedPort);
-
-            SerialPort outputPort = new SerialPort(selectedPort, 9600);
-
-            outputPort.DataReceived += OutputPort_DataReceived;
-
-            outputPort.Open();
-
             outputPort.Write("\rRM\r");
 
             byte checksum = 0;
 
+            char lastCh = ' ';
+
             foreach(char ch in codeEditTextBox.Text)
             {
-                checksum += (byte) ch;
-
                 if (ch == '\n')
                 {
                     // ignore linefeeds - only using CR
                     continue;
                 }
                 outputPort.Write(ch.ToString());
+
+                checksum += (byte)ch;
+                lastCh = ch;
+            }
+
+            byte[] oneByte = new byte[1];
+
+            if (lastCh != '\r')
+            {
+                // Add a terminator
+                oneByte[0] = (byte) '\r'; // terminator 
+                outputPort.Write(oneByte, 0, 1);
+                checksum += (byte)'\r';
             }
 
             // write the terminator
 
-            byte[] oneByte = new byte[1];
-
-            oneByte[0] = 97; // terminator
+            oneByte[0] = TERMINATOR; // terminator 
 
             outputPort.Write( oneByte, 0, 1);
 
             oneByte[0] = checksum; // checksum
 
             outputPort.Write(oneByte, 0, 1);
-            outputPort.Close();
         }
 
+        void sendMessage(string message)
+        {
+            if (outputPort == null)
+            {
+                MessageBox.Show("No serial port connected. Select a port and press the Connect button", "Program transfer");
+                return;
+            }
+
+            outputPort.Write(message + "\r");
+        }
+
+        void stopProgram()
+        {
+            sendMessage("RH");
+        }
+
+        void startProgram()
+        {
+            sendMessage("RS");
+        }
+
+        void pauseProgram()
+        {
+            sendMessage("RP");
+        }
+
+        void resumeProgram()
+        {
+            sendMessage("RS");
+        }
         private void OutputPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
         {
             SerialPort outputPort = sender as SerialPort;
@@ -214,6 +293,41 @@ namespace HullPixelbotCode
         private void refershPortsButton_Click(object sender, RoutedEventArgs e)
         {
             populatePorts();
+        }
+
+        private void connectButton_Click(object sender, RoutedEventArgs e)
+        {
+            connectSerial();
+        }
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            resetMonitor();
+        }
+
+        private void disconnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            disconnectSerial();
+        }
+
+        private void stopButtonClick(object sender, RoutedEventArgs e)
+        {
+            stopProgram();
+        }
+
+        private void startButtonClick(object sender, RoutedEventArgs e)
+        {
+            startProgram();
+        }
+
+        private void pauseButtonClick(object sender, RoutedEventArgs e)
+        {
+            pauseProgram();
+        }
+
+        private void resumeButtonClick(object sender, RoutedEventArgs e)
+        {
+            resumeProgram();
         }
     }
 }
